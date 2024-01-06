12d1a681be28d7dc660de21bf5afd364b87004a4
muhammadaziz01-dev <akbarovmuhammadaziz050@gmail.com>
2024-01-06 16:35:35 +0300

    JavaScript lesson-2 test

---
+++ index.js
@@ -0,0 +1,47 @@
+// a=10;
+// b=2;
+// console.log( "a kesmani ichida b kesmadamn "+ a/b + " ta joylashtirishlik mumkun");
+
+// var a = prompt("ihtiyori ikkin honalik son yozing");
+
+// console.log(a%10 + " birliklar  honasidagi son");
+// var b =(a - (a%10))  ;
+// console.log(b/10 + " onliklar  honasidagi son");
+
+// var ball = prompt("imtihon balingizni  kiriting: max = 40");
+
+// var hw = prompt("uy ishi natijalari, max = 5");
+// var dv = prompt("davomat natijalari, max = 5");
+// var natija = +ball + +hw + +dv;
+// console.log(natija);
+// if (natija >= 40);{
+//     console.log("siz imtihondan muofaqiyatli o'tdingiz");
+//     if (natija >=30);{
+//         console.log("siz imtihon natijangiz qomiqarli");
+//         if (natija <=20);{
+//             console.log("siz imtihon qayta topshiring ");
+//             if (natija <=10);{
+//                 console.log("siz imtihon yiqildingiz ");   
+//             }    
+//         }    
+//     }
+      
+// } 
+
+/*let ball = prompt("Ballarni kiriting: ");
+if (+ball <=40){
+    let hw = prompt ("Uy ishi natigalari , max:5");
+    let dv = prompt ("Davomat natigalari , max:5");
+    let maxBal = +ball +  +hw + +dv;
+    if (maxBal === 50){
+        alert ("Imtihondan muofaqiyatli o'tdingiz ! sizning natijangiz: " + maxBal);
+    }else {
+        if (maxBal <= 20 && maxBal >=10){
+            alert ("Qayta ttopshirishga qoldingoz ! sizning natijangiz: " + maxBal);
+        }else if (maxBal <= 20 && maxBal >=10) {
+             alert ("Qayta ttopshirishga qoldingoz ! sizning natijangiz: " + maxBal);
+            
+        }
+    }
+
+}*/
---
+++ redme.md
@@ -0,0 +1,16 @@
+# *JavaScript lesson-2*
+
+## 1. "use strict" 
+
+- **Bu ko'd yozsak js ni oldingi versiyalaridagi bug larni tekshirip yoki aytish mumkun aniqlab berish maqsadida ishlaydi**
+
+## 2. " typeof "
+
+- **Bu functsiya bizga malumotni qaysi turda ekanligini aniqlab beradi**
+
+**M :** 
+```
+console.log(typeof "Assalomu alekum"); //natija -> string
+console.log(typeof 571); //natija -> number
+
+```
